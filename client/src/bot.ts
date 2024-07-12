e07bf18bad9231f1fc68d8786cce231b377a49f6
jas0n2k <jas0n2k@outlook.com>
2024-07-13 07:20:22 +0800

    feat: add two new pages 'bot' and 'register'

---
+++ client/src/bot.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './stylesheets/main.css'
+import './stylesheets/shadow.css'
+import './stylesheets/color.css'
+import Bot from './views/Bot.vue'
+
+createApp(Bot).mount('#app')
---
+++ client/src/register.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './stylesheets/main.css'
+import './stylesheets/shadow.css'
+import './stylesheets/color.css'
+import Register from './views/Register.vue'
+
+createApp(Register).mount('#app')
