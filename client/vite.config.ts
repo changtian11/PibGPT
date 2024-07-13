b146fc37126bed366fb9bcf84fc9f8fae70c2639
jas0n2k <jas0n2k@outlook.com>
2024-07-13 09:03:13 +0800

    fix: remove unused uri

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,10 +12,6 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-      },
-      '/uploads': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
       }
     }
   },
