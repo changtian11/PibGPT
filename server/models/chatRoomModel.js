fd7986ee476280cbbca0282e40197e0f5537d028
jas0n2k <jas0n2k@outlook.com>
2024-07-14 05:13:14 +0800

    feat: add methods to handle new messasge and user, and update the topic

--- server/models/chatRoomModel.js
+++ server/models/chatRoomModel.js
@@ -2,12 +2,25 @@ const mongoose = require('mongoose');
 
 const chatRoomSchema = new mongoose.Schema({
     roomId: { type: String, required: true, unique: true },
-    users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Array of users in the chat room
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     messages: [{
         type: mongoose.Schema.Types.ObjectId, ref: 'ChatMessage'
-    }], // Array of chat messages
-    lastMessageTimestamp: { type: Date, default: Date.now }, // Timestamp of the last message
-    topic: { type: String, required: true }, // Topic of the chat room
+    }],
+    lastMessageTime: { type: Date, default: () => Date.now() },
+    topic: { type: String, required: true },
+}, {
+    methods: {
+        addUser(userId) {
+            this.users.push(userId);
+        },
+        addMessage(messageId) {
+            this.messages.push(messageId)
+            this.lastMessageTime = new Date();
+        },
+        updateTopic(newTopic) {
+            this.topic = newTopic;
+        }
+    }
 });
 
 module.exports = mongoose.model('ChatRoom', chatRoomSchema);
