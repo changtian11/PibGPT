69a2fbac20e80d5c7ef5679887e46c140f4a63be
jas0n2k <jas0n2k@outlook.com>
2024-07-13 09:00:08 +0800

    feat: add register user page

---
+++ client/src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+    <NMessageProvider>
+        <Transition>
+            <RegisterModal />
+        </Transition>
+        <div class="page-container">
+
+        </div>
+    </NMessageProvider>
+</template>
+
+<script setup lang="ts">
+    import RegisterModal from '../components/RegisterModal.vue';
+    import { NMessageProvider } from 'naive-ui';
+</script>
+
+<style lang="css" scoped></style>
