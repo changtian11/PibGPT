33ff3d7542341ea156f944f081d7d2c195a6e079
jas0n2k <jas0n2k@outlook.com>
2024-07-14 05:12:18 +0800

    refactor: improve the way of importing modules to reduce code lines

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-const { getPfpById } = require('../controllers/fileController');
-const { registerUser, loginUser, getUserProfile, updateUserPfp, authenticateUser } = require('../controllers/userController');
+const fileCtl = require('../controllers/fileController');
+const userCtl = require('../controllers/userController');
+const chatCtl = require('../controllers/chatController');
 
 const uploadMiddleware = require('../middleware/uploadMiddleware');
 const authMiddleware = require('../middleware/authMiddleware');
+const adminAuthMiddleware = require('../middleware/adminAuthMiddleware');
 
 
 // User register and login
-router.post('/register', uploadMiddleware, registerUser);
-router.post('/login', loginUser);
+router.post('/register', uploadMiddleware, userCtl.registerUser);
+router.post('/login', userCtl.loginUser);
 
 // Get resources
-router.get('/getPfpById/:pfpId', getPfpById);
+router.get('/getPfpById/:pfpId', fileCtl.getPfpById);
 
 
 // Privileged actions 
-router.get('/auth', authMiddleware, authenticateUser)
-router.post('/updateUserPfp', authMiddleware, updateUserPfp);
-router.get('/getUserProfile', authMiddleware, getUserProfile);
+router.get('/auth', authMiddleware, userCtl.authenticateUser)
+router.post('/update-pfp', authMiddleware, userCtl.updateUserPfp);
+router.get('/getUserProfile', authMiddleware, userCtl.getUserProfile);
+router.get('/logout', authMiddleware, userCtl.logoutUser)
+
+// Chat-related
+
+router.get('/createChatroom', authMiddleware, chatCtl.createChatRoom)
+router.get('/chat/:chatId', authMiddleware, chatCtl.getChatRoomMessageHistory)
+router.get('/chatrooms', authMiddleware, chatCtl.getUserChatRoomList);
+router.get('/all-chatrooms', authMiddleware, adminAuthMiddleware, chatCtl.getAllChatRoomList);
 
 module.exports = router;
