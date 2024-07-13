8c9e258dc6f93f8b12b151ae339ef9d2970ec582
jas0n2k <jas0n2k@outlook.com>
2024-07-13 09:17:08 +0800

    fix: password attribute now can be access from authenticate function

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     nickname: { type: String, required: false },
-    password: { type: String, required: true, select: false },
+    password: { type: String, required: true },
     role: { type: String, enum: ['user', 'bot'], required: true },
     profilePhoto: { type: mongoose.Schema.Types.ObjectId, ref: 'File' }
 });
