9fa5e263f7ef3108c76a0bdb25b06c21d11fb114
Jas0n2K <jas0n2k@outlook.com>
2024-08-04 03:48:48 +0800

    refactor(server): consolidate routes, prioritize static file serving

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,22 +4,21 @@ const path = require('path');
 const apiRoutes = require('./api');
 const staticRoutes = require('./static');
 
-//Serve entry html files
-
-router.get('/:roomId?', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'public', "index.html"));
-})
+router.use('/assets', express.static(path.join(__dirname, '..', 'public', 'assets')));
 
 router.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'public', 'register.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'register.html'));
 });
 
 router.get('/bot/:roomId?', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'public', 'bot.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'bot.html'));
 });
 
 router.use('/api', apiRoutes);
-
 router.use('/static', staticRoutes);
+
+router.get('/:roomId?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', "index.html"));
+})
 
 module.exports = router;
