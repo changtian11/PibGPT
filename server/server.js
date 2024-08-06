2af9bffc815599300ea9c29fb2fdb002b5efd7a4
jas0n2k <jas0n2k@outlook.com>
2024-08-06 18:48:46 +0800

    refactor(server): redesign config module (use Config instance, handle missing config) (chalk logging added)

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// require('dotenv').config();
 const app = require('./app');
 const { createUploadDirs } = require('./utils/fileUtil')
 const { connectDB } = require('./utils/db');
--- server/utils/config.js
+++ server/utils/config.js
@@ -1,24 +1,39 @@
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
 
-class CustomConfig {
+class Config {
     config = null;
-    defaultConfig = require('../config/default.json');
     serverDataRootPath = path.resolve(os.homedir(), 'pibgpt-server');
-    configPath = path.resolve(this.serverDataRootPath, 'config');
+    configPath = path.resolve(this.serverDataRootPath, 'config.json');
 
-    constructor() {
-        try {
-            if (!fs.existsSync(path.resolve(this.configPath, 'default.json'))) {
-                if (!fs.existsSync(this.configPath)) fs.mkdirSync(this.configPath, { recursive: true });
-                fs.writeFileSync(path.resolve(this.configPath, 'default.json'), JSON.stringify(this.defaultConfig));
-            }
-            this.config = require(path.resolve(this.configPath, 'default.json'));
+    async initialize() {
+        if (fs.existsSync(this.configPath)) {
+            this.config = require(this.configPath);
+            console.log(chalk.green(`Config loaded from existing file. (${this.configPath})`));
         }
-        catch (err) {
-            console.error(err);
-            throw err;
+        else {
+            const ans = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'JWT_SECRET',
+                    message: 'Enter your JWT secret:',
+                    validate: (input) => input.length > 0 || 'JWT secret cannot be empty'
+                },
+                {
+                    type: 'input',
+                    name: 'MONGODB_URI',
+                    message: 'Enter your MongoDB URI:',
+                    validate: (input) => input.startsWith('mongodb') || 'Invalid MongoDB URI format'
+                }
+            ]);
+            this.config = ans;
+            this.saveToFile()
+                .catch(err => {
+
+                })
         }
     }
 
@@ -28,6 +43,60 @@ class CustomConfig {
         }
     }
 
+    async set(property, value) {
+        if (!this.config) {
+            await this.initialize();
+        }
+
+        this.config[property] = value;
+
+        this.saveToFile()
+            .catch(err => {
+
+            })
+    }
+
+    async saveToFile() {
+        try {
+            if (!fs.existsSync(this.serverDataRootPath)) {
+                fs.mkdirSync(this.serverDataRootPath, { recursive: true });
+            }
+            fs.writeFileSync(this.configPath, JSON.stringify(this.config, null, 2));
+            console.log(chalk.green(`Config saved to file. (${this.configPath})`));
+        }
+
+        catch (err) {
+            console.error(chalk.red('Error saving config:'), err);
+            throw err;
+        }
+    }
 }
 
-module.exports = new CustomConfig();
+async function handleExit(signal) {
+    try {
+        await config.saveToFile();
+        console.log(chalk.blue(`Configuration saved. Exiting due to ${signal}...`));
+        process.exit(0);
+    } catch (err) {
+        console.error(chalk.red(`Error saving config on exit (${signal}):`), err);
+        process.exit(1);
+    }
+}
+
+const config = new Config();
+
+// IIFE
+(async () => {
+    try {
+        await config.initialize();
+    }
+    catch (err) {
+        console.error(chalk.red("Failed to init config: ", err));
+        process.exit(1);
+    }
+
+    process.on('SIGINT', () => handleExit('SIGINT'));
+    process.on('SIGTERM', ()=> handleExit('SIGTERM'));
+})
+
+module.exports = new Config();
