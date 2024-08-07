5644568528df9257e96da91fa8bfb07e582a1a9a
jas0n2k <jas0n2k@outlook.com>
2024-08-07 11:56:22 +0800

    refactor(server): update connectDB() to use promises and add retry logic

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose');
 const config = require('./config');
-const MONGODB_URI = config.get('MONGODB_URI')
+const logger = require('./logger')('db');
 
+const MAX_RETRY_ATTEMPTS = 5;
+const RETRY_DELAY = 5000;
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(MONGODB_URI, {
-            dbName: MONGODB_DBNAME
-        });
-    }
-    catch (error) {
-        console.error(`MongoDB connection failed: ${error}`);
-        process.exit(1);
-    }
+function connectDB(attempt = 1) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            await mongoose.connect(config.get('MONGODB_URI'));
+            logger.info('Connected to MongoDB.')
+            resolve();
+        }
+        catch (err) {
+            logger.error(`MongoDB connection failed: ${err}`);
+            if (attempt < MAX_RETRY_ATTEMPTS) {
+                logger.info(`Retrying connection in ${RETRY_DELAY / 1000} seconds... (Attempt ${attempt + 1})`);
+                setTimeout(() => connectDB(attempt + 1).then(resolve).catch(reject), RETRY_DELAY);
+            } else {
+                reject(new Error('Failed to connect to MongoDB Max retry attempts reached'));
+            }
+        }
+    })
 }
 
 module.exports = { connectDB };
